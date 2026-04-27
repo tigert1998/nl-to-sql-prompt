89fa4bbdb65351c5f131747dea4e3a70119aaecd
tigertang <tigertang.zju@qq.com>
2026-04-27 18:08:54 +0800

    Update prompt

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import os
+
+
 def generate_prompt():
     with open("./schema.sql", "r") as f:
         schema = f.read()
+
+    ls = []
+    for filename in os.listdir("./values"):
+        if not filename.lower().endswith(".csv"):
+            continue
+        table, column, _ = filename.split(".")
+        with open(os.path.join("values", filename), "r") as f:
+            content = f.read()
+        ls.append(f"`{table}`表的`{column}`列仅包含如下取值：\n```csv\n{content}```\n")
 
     return f"""# 角色
 你是SQL编写机器人，能根据用户的查询需求生成正确无误的SQL语句。
@@ -11,11 +23,15 @@ def generate_prompt():
 {schema}
 ```
 
+数据表格中部分字段仅能从有限的值中选择，务必在构建SQL时不要超出给定的取值列表，下面是这些列的可选值列表：
+
+{"\n".join(ls)}
+
 # 用户查询
 {{query}}
 
 # 任务
-在用户查询能够实现的情况下，根据用户查询和数据表格结构，生成正确的SQL查询语句。
+在用户查询能够实现的情况下，根据用户查询和数据表格结构，生成正确的SQL查询语句。现在的时间是{{time}}，如果用户查询涉及时间且没有明确指定年份，则默认指代今年，你需要在SQL语句中指定为今年。
 
 # 格式
 必须严格按照XML格式输出，XML的顶部节点名字必须为`output`。如果用户查询与数据库无关，则继续生成`success`标签为0的XML，例如：
